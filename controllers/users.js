4523c3e07ef9827da3a9900e5ed181eed2253fc9
Abhinavkumar2025 <abhinavku04@gmail.com>
2025-12-04 00:10:23 +0530

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -4,23 +4,40 @@ module.exports.renderRegister = (req, res) => {
     res.render('users/register')
 }
 
-module.exports.register = async(req, res,next) => {
-    try{
-        const {email,password,username} = req.body;
-        const user = new User({username,email});
-        const registeredUser = await User.register(user,password);
-        req.login(registeredUser, err =>{
-            if(err) return next(err);
-            req.flash('success', "Welcome to yelp camp");
-            res.redirect('/campgrounds');
-        })
+module.exports.register = async (req, res, next) => {
+    const { email, password, username } = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+
+    // Wrap req.login in a Promise
+    await new Promise((resolve, reject) => {
+        req.login(registeredUser, err => {
+            if (err) return reject(err);
+            resolve();
+        });
+    });
+
+    req.flash('success', "Welcome to YelpCamp");
+    res.redirect('/campgrounds');
+};
+
+// module.exports.register = async(req, res,next) => {
+//     try{
+//         const {email,password,username} = req.body;
+//         const user = new User({username,email});
+//         const registeredUser = await User.register(user,password);
+//         req.login(registeredUser, err =>{
+//             if(err) return next(err);
+//             req.flash('success', "Welcome to yelp camp");
+//             res.redirect('/campgrounds');
+//         })
         
-    }
-    catch(e){
-        req.flash('error',e.message);
-        res.redirect('register');
-    }
-}
+//     }
+//     catch(e){
+//         req.flash('error',e.message);
+//         res.redirect('register');
+//     }
+// }
 
 module.exports.renderLogin = (req, res) => {
     res.render('users/login');
